037d6fbb630521774cf00b85861b243da9ac41c8
AmitKumar <just4uamitkumar@gmail.com>
2025-07-31 17:32:01 +0530

    removed extra files

--- src/components/pages/Auth/enum.ts
+++ src/components/pages/Auth/enum.ts
@@ -1,10 +1,12 @@
-export enum AuthEnum {
-    fillAllFields = 'Please fill all fields',
-    invalidFName = 'Please fill a valid first name',
-    invalidLName = 'Please fill a valid last name', 
-    sameName = 'First name and last name cannot be the same', 
-    invalidEmail = 'Please enter a valid email',
-    existEmail = 'Email is already registered',
-    invalidPassword = 'Please enter a valid password', 
-    notSamePassword = 'confirm password and password are not same',
-}
+export const AuthEnum = {
+    fillAllFields: 'Please fill all fields',
+    invalidFName: 'Please fill a valid first name',
+    invalidLName: 'Please fill a valid last name', 
+    sameName: 'First name and last name cannot be the same', 
+    invalidEmail: 'Please enter a valid email',
+    existEmail: 'Email is already registered',
+    invalidPassword: 'Please enter a valid password', 
+    notSamePassword: 'confirm password and password are not same',
+} as const;
+
+export type AuthEnum = typeof AuthEnum[keyof typeof AuthEnum];
